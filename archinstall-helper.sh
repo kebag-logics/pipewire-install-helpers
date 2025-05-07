5db8365bda6cb18b2f391ca7df22e9153b57badf
hackerman-kl <alexandre.malki@kebag-logic.com>
2025-05-07 12:44:39 +0200

    adding configuration urls

--- archinstall-helper.sh
+++ archinstall-helper.sh
@@ -1,6 +1,8 @@
 #!bin/sh
 
-curl --verbose -g yourlongurlhere -o /tmp/user_configuration.json 
-curl --verbose -g yourlongurlhere -o /tmp/user_credentials.json
+# Retrieve the configurations
+curl --verbose -g https://raw.githubusercontent.com/kebag-logics/pipewire-install-helpers/refs/heads/main/user_configuration.json -o /tmp/user_configuration.json
+curl --verbose -g https://raw.githubusercontent.com/kebag-logics/pipewire-install-helpers/refs/heads/main/user_credentials.json -o /tmp/user_credentials.json
 
+# Execute the script 
 archinstall --config /tmp/user_configuration.json --creds /tmp/user_credentials.json
